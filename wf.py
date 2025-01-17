d3e7b7e4254afdc945e17890bb180700f1978ced
Aravindhakshan S <86773708+A-Dhakshan@users.noreply.github.com>
2025-01-17 17:41:32 +0530

    added comments

--- wf.py
+++ wf.py
@@ -1,6 +1,7 @@
 import tifffile
 import matplotlib.pyplot as plt
 
+#reads and plots the widefield image output of SOFI simulation tool
 WF = tifffile.imread("raw_files/widefield.tif")
 plt.imshow(WF, cmap='hot')
 plt.axis("Off")
